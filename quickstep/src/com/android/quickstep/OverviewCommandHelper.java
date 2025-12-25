836310b57419564412ce8ca87e0f0f793db5b6f0
IamCOD3X <sourabh.panchal28@gmail.com>
2025-12-25 16:00:42 +0530

    Merge cherrypicks of ['googleplex-android-review.googlesource.com/35238811'] into udc-platform-release.
    Change-Id: Id7efa5ac1b3d6760c16e407dc79c3fe9f239dc2c

--- quickstep/src/com/android/quickstep/OverviewCommandHelper.java
+++ quickstep/src/com/android/quickstep/OverviewCommandHelper.java
@@ -25,6 +25,7 @@ import android.content.Intent;
 import android.graphics.PointF;
 import android.os.SystemClock;
 import android.os.Trace;
+import android.view.KeyEvent;
 import android.view.View;
 
 import androidx.annotation.BinderThread;
@@ -72,6 +73,7 @@ public class OverviewCommandHelper {
     private final TouchInteractionService mService;
     private final OverviewComponentObserver mOverviewComponentObserver;
     private final TaskAnimationManager mTaskAnimationManager;
+    private final SystemUiProxy mSystemUiProxy;
     private final ArrayList<CommandInfo> mPendingCommands = new ArrayList<>();
 
     /**
@@ -91,10 +93,12 @@ public class OverviewCommandHelper {
 
     public OverviewCommandHelper(TouchInteractionService service,
             OverviewComponentObserver observer,
-            TaskAnimationManager taskAnimationManager) {
+            TaskAnimationManager taskAnimationManager,
+            SystemUiProxy systemUiProxy) {
         mService = service;
         mOverviewComponentObserver = observer;
         mTaskAnimationManager = taskAnimationManager;
+        mSystemUiProxy = systemUiProxy;
     }
 
     /**
@@ -238,7 +242,7 @@ public class OverviewCommandHelper {
                 case TYPE_HOME:
                     ActiveGestureLog.INSTANCE.addLog(
                             "OverviewCommandHelper.executeCommand(TYPE_HOME)");
-                    mService.startActivity(mOverviewComponentObserver.getHomeIntent());
+                    mSystemUiProxy.onKeyEvent(KeyEvent.KEYCODE_HOME);
                     return true;
                 case TYPE_SHOW:
                     // When Recents is not currently visible, the command's type is TYPE_SHOW
